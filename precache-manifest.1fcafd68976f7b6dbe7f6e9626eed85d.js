1dcdd4f7ae29b79f36cb5c3cdecd59fb01e9c253
ray.battle <rbattle>
2020-03-28 15:18:50 -0400

    Updates

---
+++ precache-manifest.1fcafd68976f7b6dbe7f6e9626eed85d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f5843247b4e3c706c36af5f958fed3",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fa3552ad235ffcd75ccc",
+    "url": "/github.io/monsters-rolodex/static/css/main.4995e84a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3552ad235ffcd75ccc",
+    "url": "/github.io/monsters-rolodex/static/js/main.d79348b1.chunk.js"
+  },
+  {
+    "revision": "946c2ffd7f3a853f287e",
+    "url": "/github.io/monsters-rolodex/static/js/runtime-main.f04d00d0.js"
+  }
+]);
